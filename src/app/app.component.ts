5cc33969ae7816abf0dd56a27cc2d606c5978f48
iwonaive <szymaniak.iwona11@gmail.com>
2024-02-10 20:07:55 +0100

    after CR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent implements OnInit {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
-  loginForm!: FormGroup;
-  ngOnInit() {
-    this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required),
-    });
-  }
-  onSubmit() {
-    if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-    }
+export class AppComponent {
+  title = 'plane-seats-booking-app';
+  constructor(translate: TranslateService) {
+    translate.addLangs(['en']);
+    translate.setDefaultLang('en');
+    translate.use('en');
   }
 }
