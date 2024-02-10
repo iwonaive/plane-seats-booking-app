37536433d910498a26f99739832e804312676edd
iwonaive <szymaniak.iwona11@gmail.com>
2024-02-10 21:43:43 +0100

    after all reReCR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { FormComponent } from './pages/form/form.component';
 import { ListComponent } from './pages/list/list.component';
 
 const routes: Routes = [
-  { path: 'form', component: FormComponent },
-  { path: 'list', component: ListComponent },
   { path: '', redirectTo: '/form', pathMatch: 'full' },
   { path: '**', redirectTo: '/form', pathMatch: 'full' },
+  { path: 'form', component: FormComponent },
+  { path: 'list', component: ListComponent },
 ];
 
 @NgModule({
